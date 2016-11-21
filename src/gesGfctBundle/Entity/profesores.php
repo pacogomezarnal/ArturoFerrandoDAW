5b555fd20579fb2edd224734ed0ac34e8d2c2568
arturet4 <arturet_vcf@hotmail.com>
2016-11-21 11:32:40 +0100

    actividad1

---
+++ src/gesGfctBundle/Entity/profesores.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace gesGfctBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * profesores
+ *
+ * @ORM\Table(name="profesores")
+ * @ORM\Entity(repositoryClass="gesGfctBundle\Repository\profesoresRepository")
+ */
+class profesores
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apellidos", type="string", length=255)
+     */
+    private $apellidos;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="departamento", type="integer")
+     */
+    private $departamento;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return profesores
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set apellidos
+     *
+     * @param string $apellidos
+     *
+     * @return profesores
+     */
+    public function setApellidos($apellidos)
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    /**
+     * Get apellidos
+     *
+     * @return string
+     */
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    /**
+     * Set departamento
+     *
+     * @param integer $departamento
+     *
+     * @return profesores
+     */
+    public function setDepartamento($departamento)
+    {
+        $this->departamento = $departamento;
+
+        return $this;
+    }
+
+    /**
+     * Get departamento
+     *
+     * @return int
+     */
+    public function getDepartamento()
+    {
+        return $this->departamento;
+    }
+}
+
